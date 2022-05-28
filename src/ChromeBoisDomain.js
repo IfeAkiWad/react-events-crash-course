c46ed58eb0e78da20906e09d8d011eaee674a5af
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-05-28 00:46:04 -0500

    Add an onClick  event listener to the <canvas>

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,7 +10,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-    drawChromeBoiAtCoords(event)
+    drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
@@ -27,7 +27,7 @@ export default class ChromeBoisDomain extends Component {
   
   render() {
     return (
-      <canvas 
+      <canvas onClick={toggleCycling}
         onMouseMove={this.handleMouseMove}
         width='900'
         height='600'
